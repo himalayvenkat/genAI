8356f9558784c66b194be8f7be69f8c224aad034
himalayvenkat <himalayvenkat1710@gmail.com>
2026-04-28 22:42:57 +0530

    Day 1 Files

---
+++ d1.py
@@ -0,0 +1,13 @@
+import sys
+# the below line shows the version of python you are using
+print(sys.version) 
+print("Hello World")
+print("Hi there, how are you?")
+print("I am fine,\n thank you for asking.")
+# \n is used to print the next statement in a new line
+
+#python -m venv .superman
+# the above line is used to create a virtual environment named superman
+
+# in requriments.txt file we have the list of all the packages that we need to install in our virtual environment
+#to install that packages we need to enter the code pip install -r requirements.txt in the terminal
