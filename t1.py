0da2c003f90a84589f1dde50948517ce9b2c1450
koushthubha123 <217y1a6712@mlritm.ac.in>
2026-07-06 23:08:50 +0530

    threads part 2

--- t1.py
+++ t1.py
@@ -1,12 +1,16 @@
 # threading Basics
 
 import threading
+import time
 
 def work1():
     print("This is work 1")
 
 def work2():
     print("this is work2")
+    time.sleep(10)
+    # the time.sleep makes the function sleep for the 10 seconds
+
 
 t1 = threading.Thread(target=work1)
 t5 = threading.Thread(target=work2)
@@ -16,10 +20,11 @@ t4 = threading.Thread(target=work1)
 
 
 t1.start()
+t5.start()
 t2.start()
 t3.start()
 t4.start()
-t5.start()
+
 
 
 t1.join()
