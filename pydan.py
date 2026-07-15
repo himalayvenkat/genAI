119ed6dcb0e290f7e4c637a2609d04ffef059acb
koushthubha123 <217y1a6712@mlritm.ac.in>
2026-07-15 23:42:31 +0530

    pydan Optional error Fix

--- pydan.py
+++ pydan.py
@@ -3,7 +3,7 @@
 
 
 from pydantic import BaseModel
-
+from typing import Optional
 class User(BaseModel):
     id: int
     name: str
@@ -48,3 +48,28 @@ class x3(BaseModel):
 
 xy = x3(age = 10,name="Himala")
 print(xy)
+
+
+# Pydantic Installation 
+
+# 1. Open cmd  
+# C:\Users\XXXX\Documents>mkdir pydantic_project
+
+# C:\Users\XXXX\Documents>cd pydantic_project
+
+# C:\Users\XXXX\Documents\pydantic_project>python -m venv pydantic_venv
+
+# C:\Users\XXXX\Documents\pydantic_project>pydantic_venv\Scripts\activate
+
+# (pydantic_venv) C:\Users\XXXX\Documents\pydantic_project>pip install pydantic
+
+# (pydantic_venv) C:\Users\XXXX\Documents\pydantic_project>python
+# Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> import pydantic
+# >>> print(pydantic.__version__)
+# 2.13.4
+# >>> exit()
+
+# (pydantic_venv) C:\Users\XXXX\Documents\pydantic_project>deactivate ----> Deactivating virtual env
+# C:\Users\XXXX\Documents\pydantic_project>
