71613cc902a205759919494349024ccec55b7e2f
koushthubha123 <217y1a6712@mlritm.ac.in>
2026-07-18 23:48:35 +0530

    pydan 3,4

--- pydan.py
+++ pydan.py
@@ -140,3 +140,198 @@ data1 = {
 
 xy1 = B2(**data1)
 print(xy1)
+
+# Field Validator
+
+
+### this is an error program 
+# from pydantic import BaseModel,field_validator
+# class U1(BaseModel):
+#     username: str
+#     password: str
+
+#     @field_validator('username')
+#     def use(cls,v):
+#         if(len(v)<4):
+#             raise ValueError("Min length >4")
+#         return v
+# data = {'username': "Hi",'password':"GTGT"}   
+# x = U1(**data)
+# print(x)
+
+
+## its an error
+# from pydantic import BaseModel,field_validator
+# class U11(BaseModel):
+#     username: str
+#     password: str
+
+#     @field_validator('username','password')
+#     def cdc(cls,v):
+#         if (len(v)<4):
+#             raise ValueError("Its an error")
+#         return v
+
+# x = U11(username ="HIHIH",password='HUH')
+
+from pydantic import BaseModel,field_validator
+class U12(BaseModel):
+    username: str
+    password: str
+
+    @field_validator('username')
+    def x1(cls,v):
+        if (len(v)<4):
+            raise ValueError('Hi its an error of one')
+        return v
+xx = U12(username = 'HIHIHI',password = 'JUJUJU')    
+print(xx)
+
+from pydantic import BaseModel,model_validator
+class U13(BaseModel):
+    username: str
+    password: str
+
+    @model_validator(mode = 'after')
+    # the below one is depricated
+    def cec(cls,values):
+        if (values.username == values.password):
+            raise ValueError("Username and password can not be same")
+        return values
+# x = U13(username = 'HIHIHI',password = "HIHIHI") ---> This will give error
+x = U13(username = 'HIHIHIUHU',password = "HIHIHI")
+print(x)
+
+
+from pydantic import BaseModel,model_validator
+class U13(BaseModel):
+    username: str
+    password: str
+
+    @model_validator(mode = 'after')
+    # instance 
+    def cec(self):
+        if (self.username == self.password):
+            raise ValueError("Username and password can not be same")
+        return self
+# x = U13(username = 'HIHIHI',password = "HIHIHI") ---> This will give error
+x = U13(username = 'HIHIHIUHU',password = "HIHIHI")
+print(x)
+
+from pydantic import BaseModel,Field,computed_field
+class book(BaseModel):
+    id: int
+    roomid: int
+    rate : int
+    days: int = Field(
+        ...,
+        ge =1
+    )
+
+    @computed_field
+    @property
+    def totalBill(self)-> float:
+        return self.rate*self.days
+    
+x = book(id = 1,roomid = 123,rate = 1000,days = 5)
+
+print(x)
+print(x.model_dump())
+print(x.totalBill)
+from typing import Annotated
+from pydantic import BaseModel,field_validator,model_validator,BeforeValidator
+
+
+def priceConvert(value):
+        if '$' in value:
+            value.replace('$','')
+        elif ',' in value:
+            value.replace(',','')
+        return value
+
+
+class u111(BaseModel):
+    id: int
+    name: str
+    email : str
+    price: Annotated[str,BeforeValidator(priceConvert)]
+
+    @field_validator('id')
+    def idValue(cls,v):
+        if (v>100):
+            raise ValueError('The id value can not be greater than 100')
+        return v
+    
+    @model_validator(mode = 'before')
+    @classmethod
+    def valCheck(self,values):
+        if values["id"] <10 and len(values["email"])<5:
+            raise ValueError('The ID is less than 10 and the email length is less than 5')
+        return values
+
+    
+x = u111(id = 30,name = 'Himalay',email = 'Himalay@gmail.com',price='$5.44')
+
+print(x)
+
+from typing import Annotated
+from pydantic import BaseModel,BeforeValidator,AfterValidator
+
+
+def check1(value):
+    print(value)
+    print('check pass1')
+    return value
+
+def check2(value):
+    print(value)
+    print('pass2')
+    return value
+
+class U32(BaseModel):
+    id: int
+    name: str
+    email: Annotated[str,BeforeValidator(check1)]
+    Pass: Annotated[str,AfterValidator(check2)]
+
+x = U32(id=123,name="Hima",email = "Emails@gmail.com",Pass = "Gygy")
+print(x)
+
+
+from typing import List,Optional
+from pydantic import BaseModel
+
+class U11(BaseModel):
+    id: str
+    name: str
+    relationU11: Optional[List['U11']]=None
+
+U11.model_rebuild()
+x = U11(id = '1',name='sgtg',relationU11 = [U11(id = '1.1',name = "Pakodi"),U11(id = '1.2',name = "Kodi"),U11(id = '1.3',name = "Pakodi123",relationU11 = [U11(id = '1.3.1',name = "Kodi")])])
+print(x)
+
+from typing import Optional
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    street: str
+    lane_no: Optional[str]=None
+    PIN: int
+
+class User(BaseModel):
+    id: int
+    name: str
+    address : Address
+
+data = {
+    'id' : 1,
+    'name' : "Himalay",
+    'address': {
+        'street' : "st111",
+        'PIN': 500056
+    }
+}
+
+x = User(**data)
+print(x)
+
